6e25ef84dfbe727bf20c323355ee764b392a51a9
Mohammad Hafifi Bin Abu Bakar <mohammadhafifi.abubakar@gmail.com>
2025-07-16 10:44:25 +0800

    keep

--- vantara/src/commands/date/src/main.rs
+++ vantara/src/commands/date/src/main.rs
@@ -1,3 +1,14 @@
+use chrono::Local;
+use vantara::{safe_println, safe_eprintln};
+
 fn main() {
-    
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() == 1 {
+        // Tiada argumen: papar tarikh & masa sekarang
+        let now = Local::now();
+        safe_println(format_args!("{}", now.format("%a %b %e %T %Z %Y")));
+    } else {
+        safe_eprintln(format_args!("Usage: {} [no arguments]", args[0]));
+    }
 }
